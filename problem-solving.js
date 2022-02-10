a9d0bba7a29c5e56bd7a96c9aedc0961d0da2693
Ashraful <ashrafulislam2880@gmail.com>
2022-02-11 00:24:56 +0600

    Final Commit

--- problem-solving.js
+++ problem-solving.js
@@ -9,20 +9,27 @@ function cardDistribution(val) {
     const length=val.length;
     const random="00000";
     const numbers= new Array(length).fill(1).map( (_, i) => i+1 )
-    
 
-    for (const element of val){
-        const district=element.district.slice(0,2).toUpperCase();
-        const year=element.currentYear.toString().slice(2,4);
-        const post=element.postNo.toString().slice(0,2);
-        const birthDate=element.birthYear;
-        const digits= numbers.map(number=> random+number)
-        const output=district+post+birthDate+year+digits;
-        const result=output.slice(0,16);
-        // const numbers=[...Array(2).keys()]
-        
-
-        console.log( result);
-    }
+    const res= val.map((i,index)=>{
+        const district=i.district.slice(0,2).toUpperCase();
+        const year=i.currentYear.toString().slice(2,4);
+        const post=i.postNo.toString().slice(0,2);
+        const birthDate=i.birthYear;
+        const output=district+post+birthDate+year;
+        const number=random+(index+1);
+        const result=output+number;
+        return result;
+    })
+    const final=res.map(item=>{
+        const str=item.slice(0,2);
+        const number=parseInt(item.slice(2,16));
+        const obg={cardNumber:item,gift:"",pririoty:""};
+        if(number%2==0){
+            obg.gift="R"
+        }else{
+            obg.gift="W"
+        }
+        console.log(obg);
+    });
 }
 cardDistribution(task);
